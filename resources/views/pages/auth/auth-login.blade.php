2501184c9788cc1d118195bc65001910f5946ae9
Mochamad Wahyu <wahyu212122@gmail.com>
2023-09-27 00:16:27 +0700

    sedder

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -54,19 +54,7 @@
                         please fill in your password
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox"
-                            name="remember"
-                            class="custom-control-input"
-                            tabindex="3"
-                            id="remember-me">
-                        <label class="custom-control-label"
-                            for="remember-me">Remember Me</label>
-                    </div>
-                </div>
-
+                
                 <div class="form-group">
                     <button type="submit"
                         class="btn btn-primary btn-lg btn-block"
@@ -75,21 +63,7 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
+           
 
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -12,23 +12,23 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function () {
-    return view('pages.app.dashboard-siakad',['type_menu' => '']);
-})->name('register');
+// Route::get('/', function () {
+//     return view('pages.app.dashboard-siakad',['type_menu' => '']);
+// })->name('register');
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})->name('register');
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })->name('register');
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('/forgot');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('/forgot');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('/reset-password');
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('/reset-password');
 
 
